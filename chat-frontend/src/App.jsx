96ece18b95ed916fd061dde13632a825e29af1a1
Neha Manghnani <nehajm@tamu.edu>
2026-04-26 23:36:12 -0500

    login and signup frontend pages commit

--- chat-frontend/src/App.jsx
+++ chat-frontend/src/App.jsx
@@ -1,10 +1,46 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import Navbar from './components/Navbar'
+import { Route, Routes } from "react-router-dom"
+
+import { useAuthStore } from './store/useAuthStore'
+import { Loader } from "lucide-react";
+import HomePage from './pages/HomePage';
+import SignUpPage from './pages/SignUpPage';
+import SettingsPage from './pages/Settings';
+import ProfilePage from './pages/Profile';
+import { Navigate } from 'react-router-dom';
 
 const App = () => {
+  const { authUser, checkAuth, isCheckingAuth} = useAuthStore();
+
+  useEffect(() => {
+    checkAuth();
+  }, [checkAuth]);
+
+  console.log("Authenticated User:", authUser);
+
+  if(isCheckingAuth && !authUser) {
+    return (
+      <div className='flex items-center justify-center h-screen'>
+        <Loader className="size-10 animate-spin"/>
+      </div>
+    )
+  }
+
+
   return (
-    <div className='text-red-500'>
-      Hello WOrld
+    <div>
+        <Navbar />
+
+        <Routes>
+          <Route path="/" element={authUser ? <HomePage /> : <Navigate to="/login" />} />
+          <Route path="/signup" element={!authUser ? <SignUpPage /> : <Navigate to="/" />} />
+          <Route path="/login" element={!authUser ? <LoginPage /> : <Navigate to="/" />} />
+          <Route path="/settings" element={<SettingsPage />} />
+          <Route path="/profile" element={authUser ? <ProfilePage /> : <Navigate to="/login" />} />
+        </Routes>
     </div>
+    
   )
 }
 
---
+++ chat-frontend/src/pages/SignUpPage.jsx
@@ -0,0 +1,71 @@
+import React from 'react'
+import { useState } from 'react'
+import { useAuthStore } from '../store/useAuthStore'
+import { MessageSquare } from 'lucide-react'
+
+const SignUpPage = () => {
+
+const [showPassword, setShowPassword] = useState(false);  
+const [formData, setFormData] = useState({
+  username: '',
+  email: '',
+  password: '',
+});
+
+const [signup, isSigningUp] = useAuthStore();
+
+const validateForm = () => {}
+
+const handleSubmit = (e) => {
+    e.preventDefault();
+}
+
+  return (
+    <div className='min-h-screen grid lg:grid-cols-2'>
+      
+      <div className='flex flex-col justify-center items-center p-6 sm:p-12'>
+        <div className='w-full max-w-md space-y-8'>
+
+            <div className='text-center mb-8'> 
+                <div className='flex flex-col items-center gap-2 group'>
+
+                    <div className='size-12 rounded-xl bg-primary/10 flex items-center justify-center 
+                            group-hover:bg-primary/20 transition-colors'>
+
+                                <MessageSquare className='size-6 text-primary' />
+                        </div>
+
+                    <h1 className='text-2xl font-bold mt-2'>Create Account</h1>
+                    <p className='text-base-content/60'> Create your account</p>
+                </div>
+            </div>
+
+            <form onSubmit={handleSubmit} className='space-y-6'>
+                {/* Full Name */}
+                <div className='form-control'>
+                    <label className='label'>
+                        <span className='label-text font-medium'>Full Name</span>
+                    </label>
+                    <div className='relative'>
+                        <div className='absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none'>
+                            <User className='size-5 text-base-content/60' />
+                            
+                        </div>
+                        <input
+                            type='text'
+                            placeholder='Neha Manghnani'
+                            className='input input-bordered w-full pl-10'
+                            value={formData.fullName}
+                            onChange={(e) => setFormData({ ...formData, fullName: e.target.value })}
+                        />
+                    </div>
+                </div>
+            </form>
+        </div>
+      </div>
+
+    </div>
+  )
+}
+
+export default SignUpPage
